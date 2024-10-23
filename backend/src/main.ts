18e35f715ceaaa4ae533bf8b03471e8ff6e358b5
DongHyeonWon <dhe7700@naver.com>
2024-10-23 10:29:18 +0900

    init: cors 설정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,11 @@ import { setupSwagger } from 'configs/setupSwagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   setupSwagger(app);
   await app.listen(3000);
 }
