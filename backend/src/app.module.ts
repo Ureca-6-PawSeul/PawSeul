cc62d9351dc01259c10f9f95894a529a6ccac877
DongHyeonWon <dhe7700@naver.com>
2024-10-23 09:58:23 +0900

    init: typeorm 설정

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeORMConfig } from 'configs/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: `.env.${process.env.NODE_ENV}`,
+    }),
+    TypeOrmModule.forRootAsync({
+      useClass: typeORMConfig,
     }),
   ],
   controllers: [AppController],
