62fbb223a1e63c343cbf61a7ff5979c32c14d48a
chanbyeol <cksquf98@naver.com>
2024-10-25 13:30:47 +0900

    chore(fe): typo 설정 수정(LUP-45)

--- frontend/src/styles/typo.tsx
+++ frontend/src/styles/typo.tsx
@@ -3,14 +3,7 @@ import { css } from '@emotion/react';
 export const calcRem = (px: number) => `${px / 16}rem`;
 
 export const typo = {
-  Web: {
-    Heading1_Eng: css`
-      font-family: 'Pretendard';
-      font-weight: 800;
-      font-size: ${calcRem(36)};
-      line-height: 122.5%;
-    `,
-    Heading1_Kor: css`
+    Heading1: css`
       font-family: 'Pretendard';
       font-weight: 600;
       font-size: ${calcRem(36)};
@@ -49,81 +42,25 @@ export const typo = {
     Body3: css`
       font-family: 'Pretendard';
       font-weight: 500;
-      font-size: ${calcRem(14)};
+      font-size: ${calcRem(13)};
       line-height: 170%;
+    `,
+    Body4: css`
+      font-family: 'Pretendard';
+      font-weight: 400;
+      font-size: ${calcRem(8)};
+      line-height: 140%;
     `,
     Label1: css`
       font-family: 'Pretendard';
       font-weight: 600;
-      font-size: ${calcRem(18)};
-      line-height: 120%;
-    `,
-    Label2: css`
-      font-family: 'Pretendard';
-      font-weight: 600;
-      font-size: ${calcRem(16)};
-      line-height: 170%;
-    `,
-    Label3: css`
-      font-family: 'Pretendard';
-      font-weight: 600;
       font-size: ${calcRem(14)};
-      line-height: 150%;
-    `,
-  },
-  Mobile: {
-    Heading1_Eng: css`
-      font-family: 'Gilroy-Bold';
-      font-size: ${calcRem(26)};
-      line-height: 140%;
-    `,
-    Heading1_Kor: css`
-      font-family: 'Pretendard';
-      font-weight: 600;
-      font-size: ${calcRem(22)};
-      line-height: 140%;
-    `,
-    Heading2: css`
-      font-family: 'Pretendard';
-      font-weight: 600;
-      font-size: ${calcRem(20)};
-      line-height: 150%;
-    `,
-    Heading3: css`
-      font-family: 'Pretendard';
-      font-weight: 600;
-      font-size: ${calcRem(18)};
-      line-height: 150%;
-    `,
-    Heading4: css`
-      font-family: 'Pretendard';
-      font-weight: 600;
-      font-size: ${calcRem(16)};
-      line-height: 150%;
-    `,
-    Body1: css`
-      font-family: 'Pretendard';
-      font-weight: 500;
-      font-size: ${calcRem(16)};
-      line-height: 170%;
-    `,
-    Body2: css`
-      font-family: 'Pretendard';
-      font-weight: 500;
-      font-size: ${calcRem(14)};
-      line-height: 170%;
-    `,
-    Label1: css`
-      font-family: 'Pretendard';
-      font-weight: 600;
-      font-size: ${calcRem(16)};
       line-height: 170%;
     `,
     Label2: css`
       font-family: 'Pretendard';
       font-weight: 600;
-      font-size: ${calcRem(14)};
-      line-height: 150%;
+      font-size: ${calcRem(13)};
+      line-height: 140%;
     `,
-  },
 } as const;
