5baffa5b66666547b205adab8e5aa4679a57b5d2
DongHyeonWon <dhe7700@naver.com>
2024-10-25 16:04:07 +0900

    [feat] entity 및 카카오 로그인 api 추가 (#11)
    * fix: tsconfig 경로 에러 수정
    
    * feat: kakao auth 추가
    
    * feat: entity 추가
    
    * feat: kakao login 추가(LUP-34)

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -22,7 +22,6 @@ export class AuthController {
     const isSignup = await this.authService.validateUser(req.user);
 
     res.setHeader('Authorization', `Bearer ${refreshToken}`);
-
     res.cookie('accessToken', accessToken, {
       httpOnly: true,
       secure: true,
