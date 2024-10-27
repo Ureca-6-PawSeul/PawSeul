4b1c27c35896dd1727da3468c3b8200a5880418e
ongheong <pudding4271@gmail.com>
2024-10-27 11:00:09 +0900

    feat: 상세목록 테이블 컴포넌트인 DetailTable.tsx 생성(LUP-47)

---
+++ frontend/src/components/store/detail/DetailTable.tsx
@@ -0,0 +1,62 @@
+import styled from '@emotion/styled';
+import { colors } from '@styles/colors';
+import { Text } from '@components/common/Typo';
+
+interface DetailTableProps {
+  tableData: Record<string, any>;
+}
+
+const DetailTable: React.FC<DetailTableProps> = ({ tableData }) => {
+  return (
+    <Table>
+      <TableBody>
+        {
+          /* tableData를 [key, value]의 배열 형태로 변환 */
+          Object.entries(tableData).map(([key, value]) => (
+            <TableRow key={key}>
+              <TableData bgColor={colors.Gray100} width="30%">
+                <Text typo="Body3" colorCode={colors.Gray500}>
+                  {key}
+                </Text>
+              </TableData>
+              <TableData bgColor={colors.White} width="70%">
+                <Text typo="Body3" colorCode={colors.Gray900}>
+                  {value}
+                </Text>
+              </TableData>
+            </TableRow>
+          ))
+        }
+      </TableBody>
+    </Table>
+  );
+};
+
+const Table = styled.table`
+  width: 100%;
+  border-collapse: collapse;
+`;
+
+const TableBody = styled.tbody`
+  display: table-row-group;
+`;
+
+const TableRow = styled.tr`
+  display: table-row;
+  vertical-align: inherit;
+  border-width: 1px;
+  border-style: solid;
+  border-color: ${colors.Gray200};
+`;
+
+const TableData = styled.td<{
+  bgColor?: string;
+  width?: string;
+}>`
+  width: ${({ width }) => width ?? `${width}`};
+  border-width: 1px;
+  padding: 8px;
+  background-color: ${({ bgColor }) => bgColor ?? `${bgColor}`};
+`;
+
+export default DetailTable;
