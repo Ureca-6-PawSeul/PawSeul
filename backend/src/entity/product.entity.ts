b168514c25cdbc004144bd86f0f3f33f48735dbf
DongHyeonWon <dhe7700@naver.com>
2024-10-28 09:46:41 +0900

    [fix] entity type 수정 (#21)
    fix: entity type 수정(LUP-10)

--- backend/src/entity/product.entity.ts
+++ backend/src/entity/product.entity.ts
@@ -21,7 +21,7 @@ export abstract class Product {
 
   @ApiProperty({ description: '제품 이미지 URL' })
   @Column()
-  product_img: string;
+  productImg: string;
 
   @ApiProperty({ description: '상세 설명 이미지 배열' })
   @Column('json')
