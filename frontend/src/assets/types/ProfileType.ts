7eee173a6ca076c249aa231cfeddb7db1575030a
chanbyeol <cksquf98@naver.com>
2024-10-29 17:40:40 +0900

    feat: Profile Component 생성(LUP-16)

--- frontend/src/assets/types/ProfileType.ts
+++ frontend/src/assets/types/ProfileType.ts
@@ -1,13 +1,13 @@
 export interface CardProfileType {
-    petname: string,
-    age: number,
-    weight: number,
-    gender: string,
-    breedname: string,
-    is_neutered: string
+  petname: string;
+  age: number;
+  weight: number;
+  gender: string;
+  breedname: string;
+  is_neutered: string;
 }
 export interface ProfileType extends CardProfileType {
-    user_id: number | string,
-    username: string,
-    pet_id: number,
+  user_id: number | string;
+  username: string;
+  pet_id: number;
 }
