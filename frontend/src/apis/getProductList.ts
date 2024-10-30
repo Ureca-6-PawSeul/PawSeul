2e70bb430707d0b1e4a6b426afc34d83eb57ed82
김찬별 <47071366+cksquf98@users.noreply.github.com>
2024-10-30 15:17:29 +0900

    [feat] Product Component height 수정, 상품페이지 2개씩 리스트로 노출되도록 변경 (#33)
    * feat: Tag Component 생성(LUP-65)
    
    * feat: align props 추가, productData를 state로 관리(LUP-65)
    
    * feat: Product Component height 수정, 상품페이지 2개씩 리스트로 노출되도록 변경(LUP-65)
    
    * fix: product type 이름 변경으로 인한 수정(LUP-59)
    
    * fix: ProductHorizontal props name 수정(LUP-65)
    
    * feat: navigator 경로 수정(LUP-65)

---
+++ frontend/src/apis/getProductList.ts
@@ -0,0 +1,9 @@
+import { Category } from '@assets/types/CategoryType';
+import client from "./client";
+
+export const getProductList = async (category, setProduct ) => {
+    console.log(`api/product/${category}`)
+    const {data} = await client.get(`api/product/${category}`)
+
+    setProduct(data)
+}
