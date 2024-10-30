475cdc393f0168f91ec4384384d39d0682790631
leejin_rho <52371699+leejin-rho@users.noreply.github.com>
2024-10-30 18:20:48 +0900

    [fix] 빌드 오타 및 에러 수정 (#38)
    * fix: 빌드 오타 및 에러 수정(LUP-73)
    
    * fix: navbar 스토리북 에러 수정(LUP-73)

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,8 +3,6 @@ import { Flex } from '@components/common/Flex';
 import { Footer } from '@components/common/Footer';
 
 const Home = () => {
-  const {isOpen, modalRef, toggleModal} = useModal;
-
   return (
     <Wrapper direction="column" align="center" justify="flex-start">
       <div></div>
