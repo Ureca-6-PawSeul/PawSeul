693816c37abed26787bb7dc591d0737848725156
Lee ye won <65122491+dldpdnjs21@users.noreply.github.com>
2024-10-31 15:13:10 +0900

    [feat(be)] mypage정보 수정 API 구현 (#43)
    feat(be): mypage정보 수정 API 구현(LUP-72)

--- backend/src/mypage/mypage.service.ts
+++ backend/src/mypage/mypage.service.ts
@@ -2,8 +2,9 @@ import { Injectable, NotFoundException, Logger } from '@nestjs/common';
 import { InjectEntityManager } from '@nestjs/typeorm';
 import { EntityManager } from 'typeorm';
 import { User } from 'src/entity/user.entity';
+import { Pet } from 'src/entity/pet.entity';
 import { GetUserResponseDto } from 'src/mypage/dto/getUserResponse.dto';
-import { UpdatePetDto } from 'src/mypage/dto/updatePet.dto';
+import { UpdatePetRequestDto } from 'src/mypage/dto/updatePetRequest.dto';
 
 @Injectable()
 export class MypageService {
@@ -20,7 +21,7 @@ export class MypageService {
     });
 
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException('유저를 찾을 수 없습니다.');
     }
 
     return {
@@ -33,7 +34,7 @@ export class MypageService {
 
   async logUserInfo(userId: string): Promise<void> {
     const user = await this.findUserById(userId);
-    this.logger.log(`User Info: ${JSON.stringify(user)}`);
+    this.logger.log(`유저 정보: ${JSON.stringify(user)}`);
   }
 
   async logPetInfo(userId: string): Promise<void> {
@@ -43,29 +44,30 @@ export class MypageService {
     });
 
     if (!user || !user.pet) {
-      this.logger.warn(`No pet found for user ID: ${userId}`);
+      this.logger.warn(`펫 정보가 없습니다.: ${userId}`);
       return;
     }
 
-    this.logger.log(`Pet Info: ${JSON.stringify(user.pet)}`);
+    this.logger.log(`펫 정보: ${JSON.stringify(user.pet)}`);
   }
 
+  //펫 정보 수정
   async updatePetInfo(
     userId: string,
-    updatePetDto: UpdatePetDto,
-  ): Promise<void> {
+    updatePetDto: UpdatePetRequestDto,
+  ): Promise<Pet> {
     const user = await this.entityManager.findOne(User, {
       where: { userId: userId },
       relations: ['pet'],
     });
 
     if (!user || !user.pet) {
-      throw new NotFoundException('Pet not found');
+      throw new NotFoundException('펫 정보를 찾을 수 없습니다.');
     }
 
-    // 반려동물 정보 업데이트
     Object.assign(user.pet, updatePetDto);
     await this.entityManager.save(user.pet);
-    this.logger.log(`Updated Pet Info: ${JSON.stringify(user.pet)}`);
+
+    return user.pet;
   }
 }
