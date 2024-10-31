0b0352baebce2b14c0b39e7a617dcfe8f18dc62c
DongHyeonWon <dhe7700@naver.com>
2024-10-31 10:51:03 +0900

    refact: base entity 수정(LUP-75)

--- backend/src/entity/base.entity.ts
+++ backend/src/entity/base.entity.ts
@@ -1,13 +1,6 @@
-import {
-  PrimaryGeneratedColumn,
-  CreateDateColumn,
-  UpdateDateColumn,
-} from 'typeorm';
+import { CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
 export abstract class BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: string;
-
   @CreateDateColumn({ type: 'timestamp' })
   createdAt: Date;
 
