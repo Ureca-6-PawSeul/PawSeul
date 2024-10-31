665a344c6e275a0b6e1fec7508bddab3cd1a4d8d
김찬별 <47071366+cksquf98@users.noreply.github.com>
2024-10-31 15:46:13 +0900

    [feat] mypage UI 주문내역 페이지 구현 (#44)
    * feat: Profile Component 생성(LUP-16)
    
    * feat: setProductDataList undefined 방지 코드(LUP-65)
    
    * chore: svg 파일 주석처리
    
    * feat: Profile Component 생성(LUP-16)
    
    * feat: setProductDataList undefined 방지 코드(LUP-65)
    
    * feat: 주문내역 Component 구현중 (LUP-16)
    
    * feat: 주문내역 OrderItems map함수 추가(LUP-16)
    
    * feat: mypage 컴포넌트 코드 수정 및 css 수정, 더미데이터 키 변경(LUP-16)
    
    * fix: reviewType 수정
    
    * chore: 변수명, inline 함수 삭제(LUP-16)

---
+++ frontend/src/pages/Mypage/Order.tsx
@@ -0,0 +1,49 @@
+import { Flex } from '@/components/common/Flex';
+import { OrderHistory } from '@components/mypage/orderHistory';
+import { SetStateAction, useEffect, useState } from 'react';
+import { getUserOrder } from '@/apis/getUserOrder';
+import { OrderHistoryType } from '@/assets/types/OrderType';
+import styled from '@emotion/styled';
+
+// 나중엔 날짜가 Date객체로 올거니까 date-fns 라이브러리 사용해서 포맷 변경해줘야 함
+export const OrderHistoryPage = () => {
+  const [userOrder, setUserOrder] = useState([]);
+  const fetch = async (
+    user_id: string,
+    setUserOrder: React.Dispatch<SetStateAction<OrderHistoryType[]>>,
+  ) => {
+    try {
+      await getUserOrder('1', setUserOrder);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    fetch('1', setUserOrder);
+  }, []);
+
+  return (
+    <Wrapper
+      direction="column"
+      justify="flex-start"
+      align="flex-start"
+      padding="0 12px"
+    >
+      {userOrder?.map((order,) => (
+        <Flex direction="column" justify="flex-start" height="fit-content">
+          <OrderHistory
+            date={order.order_created_at}
+            state={order.order_state}
+            items={order.order_items}
+          />
+        </Flex>
+      ))}
+    </Wrapper>
+  );
+};
+
+const Wrapper = styled(Flex)`
+overflow-y: auto;
+padding-bottom: 70px;
+`
