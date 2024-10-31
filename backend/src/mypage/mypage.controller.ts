2fd8aaa38df9d7791e3165f9e93ec74edb3bc1a7
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-10-31 15:09:51 +0900

    feat(be): mypage정보 수정 API 구현(LUP-72)

--- backend/src/mypage/mypage.controller.ts
+++ backend/src/mypage/mypage.controller.ts
@@ -2,9 +2,10 @@ import { Controller, Get, UseGuards, Req, Body, Patch } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { MypageService } from 'src/mypage/mypage.service';
 import { GetUserResponseDto } from 'src/mypage/dto/getUserResponse.dto';
-import { UpdatePetDto } from 'src/mypage/dto/updatePet.dto'; // DTO import
 import { ApiBearerAuth, ApiResponse } from '@nestjs/swagger';
 import { Request } from 'express';
+import { Pet } from 'src/entity/pet.entity';
+import { UpdatePetRequestDto } from 'src/mypage/dto/updatePetRequest.dto';
 
 @Controller('/api/v1/user')
 export class MypageController {
@@ -19,23 +20,33 @@ export class MypageController {
     type: GetUserResponseDto,
   })
   @ApiResponse({ status: 401, description: 'Unauthorized' })
-  async getMe(@Req() req: Request): Promise<any> {
+  async getMe(@Req() req: Request): Promise<GetUserResponseDto> {
+    const userId = req.user?.userId;
+
     // 유저 정보 로그 출력
     await this.userService.logUserInfo(req.user?.userId);
 
     // 펫 정보 로그 출력
     await this.userService.logPetInfo(req.user?.userId);
 
-    return req.user; // 유저 정보를 반환
+    return this.userService.findUserById(userId);
   }
 
   @UseGuards(AuthGuard('jwt-access'))
-  @Patch('/pet')
-  async updatePet(
+  @Patch('/me/pet')
+  @ApiBearerAuth('accessToken')
+  @ApiResponse({
+    status: 200,
+    description: 'Pet information updated successfully.',
+    type: Pet,
+  })
+  @ApiResponse({ status: 401, description: 'Unauthorized' })
+  @ApiResponse({ status: 404, description: 'Pet not found' })
+  async updatePetInfo(
     @Req() req: Request,
-    @Body() updatePetDto: UpdatePetDto,
-  ): Promise<any> {
-    await this.userService.updatePetInfo(req.user?.userId, updatePetDto);
-    return { message: 'Pet information updated successfully' };
+    @Body() updatePetDto: UpdatePetRequestDto,
+  ): Promise<Pet> {
+    const userId = req.user?.userId;
+    return this.userService.updatePetInfo(userId, updatePetDto);
   }
 }
