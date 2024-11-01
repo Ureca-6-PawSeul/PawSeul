4f775d118f4a943c93e626cd8cfedf2933431d15
chanbyeol <cksquf98@naver.com>
2024-11-01 13:40:48 +0900

    style: css 수정(LUP-70)

--- frontend/src/pages/Mypage/Review.tsx
+++ frontend/src/pages/Mypage/Review.tsx
@@ -9,6 +9,7 @@ import { Button } from '@/components/common/Button';
 import { getUserReviewDone } from '@/apis/getUserReviewDone';
 import { getUserReviewRemain } from '@/apis/getUserReviewRemain';
 import { OrderContent } from '@/components/mypage/orderHistory/orderContent';
+import { IoPersonCircleSharp } from 'react-icons/io5';
 
 export const ReviewHistoryPage = () => {
   const [activeTab, setActiveTab] = useState(1);
@@ -32,39 +33,36 @@ export const ReviewHistoryPage = () => {
 
   return (
     <Flex align="flex-start" padding="0 0 50px 0">
-      <Flex
-        direction="column"
-        justify="flex-start"
-        padding="0 12px"
-        margin="12px 0"
-        gap={0}
-        width="auto"
-        height="auto"
-      >
+      <Flex direction="column" justify="flex-start" width="auto" height="auto">
         <Flex
           direction="row"
-          justify="space-evenly"
-          gap={20}
-          backgroundColor={colors.Gray50}
+          justify="flex-start"
+          gap={12}
           borderRadius={10}
-          padding="12px"
+          padding="12px 24px"
         >
+          <IoPersonCircleSharp size={60} color={colors.Gray300} />
           <Flex
             direction="column"
             justify="flex-start"
             align="flex-start"
             width="auto"
           >
-            <Text colorCode={colors.Black} typo="Label1">
-              {userReviewList.username}님
-            </Text>
-            <Text colorCode={colors.Black} typo="Label1">
+            <ProfileText colorCode={colors.Black} typo="Heading4">
+              {userReviewList.username}
+            </ProfileText>
+            <ProfileText colorCode={colors.Gray600} typo="Body3">
               {userReviewList.email}
-            </Text>
+            </ProfileText>
           </Flex>
-          <KakaoIcon width={40} />
         </Flex>
-        <Flex justify="flex-start" gap={10} margin="40px 0 8px 0">
+        <Hr />
+        <Flex
+          justify="flex-start"
+          gap={10}
+          margin="3px 0 8px 0"
+          padding="0 12px"
+        >
           <Tab
             direction="row"
             onClick={() => handleClickTab(1)}
@@ -80,36 +78,38 @@ export const ReviewHistoryPage = () => {
             <Text typo="Label1">작성한 구매후기</Text>
           </Tab>
         </Flex>
-        {userReviewList?.reviews.map((review) => (
-          <Wrapper align="flex-start" padding="12px 12px">
-            <OrderContent
-              price={review.price}
-              title={review.title}
-              bottomContent={review.state}
-              quantity={review.quantity}
-              productImg={review.product_img}
-            >
-              <Flex width="120px">
-                {activeTab === 1 ? (
-                  <Button width="103px" height="30px" borderRadius="25px">
-                    <Text typo="Label2">리뷰 작성하기</Text>
-                  </Button>
-                ) : (
-                  <Button
-                    width="103px"
-                    height="30px"
-                    borderRadius="25px"
-                    bg={colors.Gray300}
-                    fontColor={colors.White}
-                    disabled={true}
-                  >
-                    <Text typo="Label2">작성 완료</Text>
-                  </Button>
-                )}
-              </Flex>
-            </OrderContent>
-          </Wrapper>
-        ))}
+        <Flex direction="column" padding="0 12px">
+          {userReviewList?.reviews.map((review) => (
+            <Wrapper align="flex-start" padding="12px 12px">
+              <OrderContent
+                price={review.price}
+                title={review.title}
+                bottomContent={review.state}
+                quantity={review.quantity}
+                productImg={review.product_img}
+              >
+                <Flex width="120px">
+                  {activeTab === 1 ? (
+                    <Button width="103px" height="30px" borderRadius="25px">
+                      <Text typo="Label2">리뷰 작성하기</Text>
+                    </Button>
+                  ) : (
+                    <Button
+                      width="103px"
+                      height="30px"
+                      borderRadius="25px"
+                      bg={colors.Gray300}
+                      fontColor={colors.White}
+                      disabled={true}
+                    >
+                      <Text typo="Label2">작성 완료</Text>
+                    </Button>
+                  )}
+                </Flex>
+              </OrderContent>
+            </Wrapper>
+          ))}
+        </Flex>
       </Flex>
     </Flex>
   );
@@ -125,4 +125,16 @@ const Tab = styled(ClickBtn)<{ isSelected: boolean }>`
 const Wrapper = styled(Flex)`
   border: solid 1px ${colors.Gray100};
   margin: 4px;
+`;
+
+const Hr = styled.hr`
+  border: none;
+  background-color: ${colors.Gray50};
+  height: 16px;
+  width: 100%;
+  margin: 10px 0;
+`;
+
+const ProfileText = styled(Text)`
+  line-height: 140%;
 `;
