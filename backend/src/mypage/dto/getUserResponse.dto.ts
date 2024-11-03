cd19653e1072c7702296c5c28ed50c1c7d52aaa3
Yewon_Lee <65122491+dldpdnjs21@users.noreply.github.com>
2024-11-04 02:08:27 +0900

    fix: 마이페이지 dto 파일 추가(LUP-98)

---
+++ backend/src/mypage/dto/getUserResponse.dto.ts
@@ -0,0 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Pet } from 'src/entity/pet.entity';
+
+export class GetUserResponseDto {
+  @ApiProperty({ description: '사용자의 고유 ID' })
+  userId: string;
+
+  @ApiProperty({ description: '사용자 이름' })
+  username: string;
+
+  @ApiProperty({ description: '사용자 이메일' })
+  email: string;
+
+  @ApiProperty({ description: '사용자의 반려동물 정보', type: () => Pet })
+  pet: Pet;
+}
