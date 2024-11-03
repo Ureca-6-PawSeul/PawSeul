bc1b84bcb6d05d3197b0f49b6d2cbc6efad638b2
leejin_rho <tbdpapdl@gmail.com>
2024-11-04 02:14:15 +0900

    feat(fe): 변경된 api에 맞게 type 수정(LUP-46)

--- frontend/src/stores/userStore.ts
+++ frontend/src/stores/userStore.ts
@@ -3,7 +3,5 @@ import { UserStore } from '@/assets/types/UserInfoType';
 
 export const useUserStore = create<UserStore>((set) => ({
   user: null,
-  pet: null,
   setUserInfo: (user) => set({ user }),
-  setPetInfo: (pet) => set({ pet }),
 }));
