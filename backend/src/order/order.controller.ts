a63cf9f5b01f4ccdfda65c63e0c7fff1e06f4283
DongHyeonWon <dhe7700@naver.com>
2024-11-04 02:20:51 +0900

    feat: 주문 목록 조회 api 추가(LUP-25)

---
+++ backend/src/order/order.controller.ts
@@ -0,0 +1,42 @@
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import { OrderService } from './order.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { orderListResponseDto } from 'src/order/dto/orderListResponse.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
+
+@Controller('order')
+@ApiTags('order/결제 api')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Get('/list')
+  @UseGuards(AuthGuard('jwt-access'))
+  @ApiOperation({ summary: '주문 목록 조회' })
+  @ApiResponse({
+    status: 200,
+    description: '주문 목록 조회 성공',
+    type: orderListResponseDto,
+    isArray: true,
+  })
+  async getOrderList(@Req() req: Request): Promise<orderListResponseDto[]> {
+    const { userId } = req.user;
+    return this.orderService.getOrderList(userId);
+  }
+
+  @Post('/confirm')
+  @ApiOperation({ summary: '결제 승인 요청' })
+  async confirmOrder(
+    @Body('paymentKey') paymentKey: string,
+    @Body('orderId') orderId: string,
+    @Body('amount') amount: string,
+  ) {
+    return this.orderService.confirmOrder();
+  }
+
+  @Post('/temp-order')
+  @ApiOperation({
+    summary: '임시 주문(order table에 BEFORE_PAYMENT 상태로 insert)',
+  })
+  async tempOrder(@Body('products') products: any) {}
+}
