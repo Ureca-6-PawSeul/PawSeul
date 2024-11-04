1cb2069b643a77117698da7800d66b683b738d99
ongheong <pudding4271@gmail.com>
2024-11-04 20:41:56 +0900

    chore: tableData 유틸함수 수정(LUP-99)

--- frontend/src/stores/productDetailStore.ts
+++ frontend/src/stores/productDetailStore.ts
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { ProductDetailType } from '@/assets/types/ProductType';
 
 interface ProductDetailStore {
-  productDetail: ProductDetailType | null;
+  productDetail: ProductDetailType;
   setProductDetail: (product: ProductDetailType) => void;
 }
 
