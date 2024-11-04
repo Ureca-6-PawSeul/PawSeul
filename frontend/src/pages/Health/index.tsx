a26e92d9db25271b3a6263c058cd4c9eb58d0043
leejin_rho <tbdpapdl@gmail.com>
2024-11-05 01:54:00 +0900

    feat(fe): 건강 분석 버튼 width 수정(LUP-17)

--- frontend/src/pages/Health/index.tsx
+++ frontend/src/pages/Health/index.tsx
@@ -3,6 +3,7 @@ import { HealthGuideBanner } from '@/assets/images/svgs';
 import { Button } from '@/components/common/Button';
 import { Text } from '@/components/common/Typo';
 import { useNavigate } from 'react-router-dom';
+import styled from '@emotion/styled';
 
 const Health = () => {
   const navigate = useNavigate();
@@ -18,8 +19,8 @@ const Health = () => {
       margin="60px 0 0 0"
     >
       <HealthGuideBanner width="90%" />
-      <Flex width="auto" height="auto" padding="40px 0 120px 0">
-        <Button width="90%" height="50px" onClick={handleNavigate}>
+      <Flex widthPer={80} height="auto" padding="40px 0 120px 0">
+        <Button height="50px" onClick={handleNavigate}>
           <Text typo="Body1">영양분석 하러가기</Text>
         </Button>
       </Flex>
