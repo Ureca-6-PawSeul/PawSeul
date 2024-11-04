2e4091c83e46042ba4e666b3b84c0bd7b195842b
ongheong <87983309+ongheong@users.noreply.github.com>
2024-11-04 15:33:41 +0900

    [fix] 장바구니 전역상태관리 및 조회 api 연결 수정 (#77)
    * chore: 충돌 항목 해결(LUP-99)
    
    * fix: 장바구니 전역상태관리 및 조회 api 연결 수정(LUP-99)

--- frontend/src/apis/getCartList.ts
+++ frontend/src/apis/getCartList.ts
@@ -8,6 +8,5 @@ interface cartRequest {
 
 export const getCartList = async (): Promise<CartType[]> => {
   const { data } = await client.get<cartRequest>('/cart');
-  // console.log(`data 조회: ${data.carts.map((item) => item.cartProductId)}`);
   return data.carts;
 };
