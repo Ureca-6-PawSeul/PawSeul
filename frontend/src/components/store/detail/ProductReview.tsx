25db74415c1f0fece68b242ce018b5967409ded6
ongheong <pudding4271@gmail.com>
2024-11-05 09:35:12 +0900

    feat: 리뷰 작성 모달 구현, 리뷰 생성 API 연결(LUP-99)

--- frontend/src/components/store/detail/ProductReview.tsx
+++ frontend/src/components/store/detail/ProductReview.tsx
@@ -6,16 +6,86 @@ import reviews from '@/mocks/data/review.json';
 import ProductReviewItem from './ProductReviewItem';
 import { ReviewText } from '@components/store/detail/ReviewText';
 import { calculateScoreCounts } from '@/utils/scoreCounts';
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
+import { Modal } from '@/components/common/Modal';
+import { Button } from '@/components/common/Button';
+import { Text } from '@/components/common/Typo';
+import { useMutation } from '@tanstack/react-query';
+import client from '@/apis/client';
+import { useParams } from 'react-router-dom';
+import styled from '@emotion/styled';
+import { FaStar } from 'react-icons/fa';
+
+
+interface ReviewData {
+  productId: string;
+  score: number;
+  text: string;
+}
 
 const ProductReview = forwardRef<HTMLDivElement, {}>((_, ref) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [reviewScore, setReviewScore] = useState(0);
+  const [review, setReview] = useState('');
+
+  const { id } = useParams<{ id: string }>();
+
+
+  // 후기를 생성하는 Mutation 훅
+  const mutation = useMutation<void, Error, ReviewData>({
+    mutationFn: async (reviewData) => {
+      return await client.post(`/review/${id}`, reviewData);
+    },
+    onSuccess: () => {
+      alert('후기가 성공적으로 작성되었습니다.');
+    },
+    onError: (error) => {
+      console.error(error);
+      alert('후기 작성에 실패했습니다. 다시 시도해주세요.');
+    },
+  });
+
   const reviewCount = reviews.length;
   const score = reviews.reduce((acc, cur) => acc + cur.score, 0) / reviewCount;
 
   const scoreCounts = calculateScoreCounts(reviews);
 
-  const handleNewReviewClick = () => {
-    /* 버튼 클릭 시 행동 정의 */
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  const handleStarClick = (newScore: number) => {
+    setReviewScore(newScore);
+  };
+
+  const handleReviewChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setReview(e.target.value);
+  };
+  
+  const handleExit = () => {
+    setReviewScore(0);
+    setReview('');
+    toggleModal();
+  }
+  
+  const handleSubmit = () => {
+    if (reviewScore < 1) {
+      alert('별점을 입력해주세요.');
+      return;
+    }
+    if (review.trim() === '') {
+      alert('후기 내용을 입력해주세요.');
+      return;
+    }
+
+    // 서버로 전송할 데이터 객체 생성
+    const reviewData = {
+      productId: id,
+      score: reviewScore,
+      text: review,
+    };
+
+    mutation.mutate(reviewData); // useMutation으로 후기를 서버에 요청
   };
 
   return (
@@ -32,10 +102,51 @@ const ProductReview = forwardRef<HTMLDivElement, {}>((_, ref) => {
         <ReviewStats score={score} scoreCounts={scoreCounts} />
       </Flex>
       <DetailButton
-        handleButtonClick={handleNewReviewClick}
+        handleButtonClick={toggleModal}
         colorCode={colors.MainColor}
       >
         새로운 후기 작성하기
+        <Modal isOpen={isModalOpen} toggleModal={toggleModal}>
+          <Flex direction='column'>
+            <Flex margin='10px 0 10px'>
+              <Text typo="Heading4" colorCode={colors.Gray600}>별점을 선택해주세요</Text>
+            </Flex>
+            <Flex>
+            {[1, 2, 3, 4, 5].map((star) => (
+            <span
+              key={star}
+              onClick={() => handleStarClick(star)}
+              style={{ cursor: 'pointer', fontSize: '24px' }}
+            >
+              {reviewScore >= star ? <FaStar size={24} color={colors.Star} /> : <FaStar size={24} color={colors.Gray200} />}
+            </span>
+          ))}
+            </Flex>
+            <Flex direction='column' padding='10px 20px' margin="30px 0 0">
+              <Text typo="Heading4" colorCode={colors.Gray600}>자세한 후기를 알려주세요</Text>
+              <Textarea 
+                placeholder='후기를 작성해주세요'
+                value={review}
+                onChange={handleReviewChange}
+                onKeyDown={(e) => {
+                  if (e.key === ' ') {
+                    e.preventDefault(); // 스페이스바의 기본 동작을 막음
+                    e.stopPropagation(); // 스페이스바의 이벤트 전파를 막음
+                  }
+                }}
+              />
+            </Flex>
+            <Flex padding="0px 52px" margin='20px 0 10px' gap={20}>
+            <Button height='40px' bg={colors.Gray400} onClick={handleExit}>취소</Button>
+            <Button 
+            height='40px'
+            onClick={() => {
+              handleSubmit();
+              toggleModal();
+            }}>등록</Button>
+            </Flex>
+          </Flex>
+        </Modal>
       </DetailButton>
       <div />
       {reviews.map((review, index) => (
@@ -47,4 +158,18 @@ const ProductReview = forwardRef<HTMLDivElement, {}>((_, ref) => {
 
 ProductReview.displayName = 'ProductReview';
 
+const Textarea = styled.textarea`
+  width: 100%;
+  height: 200px;
+  margin: 10px 0;
+  padding: 10px;
+  border: 1px solid ${colors.Gray100};
+  border-radius: 5px;
+  resize: none;
+`;
+
 export default ProductReview;
+function useEffect(arg0: () => () => void, arg1: boolean[]) {
+  throw new Error('Function not implemented.');
+}
+
