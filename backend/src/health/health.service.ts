68611ee40b1250a053bc569bd8bc38b3ddba48bd
DongHyeonWon <dhe7700@naver.com>
2024-11-07 02:33:21 +0900

    fix: 건강 진단 api에 product 정보 추가(LUP-124)

--- backend/src/health/health.service.ts
+++ backend/src/health/health.service.ts
@@ -9,6 +9,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import OpenAI from 'openai';
 import { Health } from 'src/entity/health.entity';
 import { Pet } from 'src/entity/pet.entity';
+import { RecommandProduct } from 'src/entity/recommandProduct.entity';
 import { AiHealthRequestDto } from 'src/health/dto/aiHealthRequest.dto';
 import { Repository } from 'typeorm';
 
@@ -22,9 +23,15 @@ export class HealthService {
 
     @InjectRepository(Pet)
     private petRepository: Repository<Pet>,
+
+    @InjectRepository(RecommandProduct)
+    private recommandProductRepository: Repository<RecommandProduct>,
   ) {}
 
   async aiHealth(aiHealthDto: AiHealthRequestDto) {
+    const recommandProduct = await this.recommandProductRepository.find({
+      relations: ['product'],
+    });
     const openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY,
     });
@@ -36,12 +43,15 @@ export class HealthService {
         messages: [
           {
             role: 'system',
-            content: `You are a pet nutrition expert. Nothing is impossible for you. Based on the pet information provided by the user, please analyze and provide the following details in JSON format:
+            content: `You are a pet(dog) nutrition expert. Nothing is impossible for you. Based on the pet information provided by the user, please analyze and provide the following details in JSON format:
               - Compare the pet's body shape to the breed standard.
-              - Provide the current and recommended calorie levels.
-              - Give the current and recommended values (in grams) for carbon, protein, and fat.
+              - Provide the current and recommended pet's calorie levels.
+              - Give the current and recommended values (in grams) for carbon, protein, and fat for pet(dog).
               - Identify two deficient nutrients from the following: calories, carbon, protein, fat, vitamin A, vitamin D, vitamin E, and calcium.
               - Select one nutrient in excess and one nutrient within the optimal range.
+              - Recommend a product that meets the pet's nutritional needs. Pick one product in ${JSON.stringify(recommandProduct)}
+              - put the product id,title,price,productImg,averageScore in the recommandProduct field.
+              -
               Your answer must always be PetNutrientStatus in JSON format and do not use \`\, I will provide JSON type :
       type NutrientName = '칼로리' | '탄수화물' | '단백질' | '지방' | '비타민A' | '비타민D' | '비타민E' | '칼슘';
       type Nutrient = {
@@ -62,6 +72,13 @@ export class HealthService {
         deficientNutrients: [NutrientName, NutrientName];
         excessNutrient: NutrientName;
         optimalNutrient: NutrientName;
+        recommandProduct: {
+            productId:string;
+            title:string;
+            price:number;
+            productImg:string;
+            averageScroe:number;
+          }
       };
        `,
           },
@@ -73,7 +90,7 @@ export class HealthService {
       });
 
       // API 호출 결과 로깅
-      this.logger.log(`AI 응답: ${JSON.stringify(chatCompletion)}`);
+      // this.logger.log(`AI 응답: ${JSON.stringify(chatCompletion)}`);
 
       const answer = chatCompletion.choices[0].message.content;
 
