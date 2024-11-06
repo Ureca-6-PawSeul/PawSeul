317b71a295ce3a7095fb013de6259588297b2264
ongheong <87983309+ongheong@users.noreply.github.com>
2024-11-06 16:52:57 +0900

    [feat] 장바구니 수정, 삭제 API 연결 (#107)
    * feat: 장바구니 수정 API 연결(LUP-96)
    
    * feat: 장바구니 아이템 삭제 API 연결(LUP-96)
    
    * feat: 디바운스로 장바구니 수정 API 호출, 삭제 시 확인 모달창 띄우기 추가(LUP-96)

--- frontend/src/apis/hooks/useCartQuery.ts
+++ frontend/src/apis/hooks/useCartQuery.ts
@@ -1,17 +1,56 @@
-import { useQuery } from '@tanstack/react-query';
+import { set } from 'date-fns';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { getCartList } from '@/apis/getCartList';
 import { CartType } from '@/assets/types/CartType';
 import useCartStore from '@/stores/cartStore';
-import { useEffect } from 'react';
+import client from '../client';
 
-const useCartQuery = () => {
+interface ChangeQuantityData {
+  productId: string;
+  quantity: number;
+}
+interface ContextType {
+  previousItems?: CartType[];
+}
+
+export const useCartQuery = () => {
   // TanStack Query를 사용하여 데이터를 패칭하고, 성공 시 Zustand 상태를 업데이트
   const {data} = useQuery<CartType[], Error>({
     queryKey: ['cartItems'],
     queryFn: getCartList,
   });
-
   return data;
 };
 
-export default useCartQuery;
+export const useChangeQuantityMutation = () => {
+  const queryClient = useQueryClient();
+  return useMutation<void, Error, ChangeQuantityData, ContextType>({
+    mutationFn: async (changeData) => {
+      return await client.patch(`/cart/update`, changeData);
+    },
+    onMutate: async ({ productId, quantity }) => {
+      await queryClient.cancelQueries({ queryKey: ['cartItems']});
+      const previousItems = queryClient.getQueryData<CartType[]>(['cartItems']);
+      useCartStore.getState().deleteSelectedItems();
+      // 변경된 상품의 수량만 업데이트 -> 낙관적 업데이트
+      queryClient.setQueryData<CartType[]>(['cartItems'], (old) =>
+        old?.map((item) =>
+          item.productId === productId ? { ...item, quantity } : item
+        ) || []
+      );
+
+      return { previousItems };
+    },
+    onError: (err, _, context) => {
+      // 에러 발생 시 이전 상태로 롤백
+      if (context?.previousItems) {
+        queryClient.setQueryData(['cartItems'], context.previousItems);
+      }
+    },
+    onSuccess: () => {
+      // 성공 또는 실패 시 캐시 무효화
+      queryClient.invalidateQueries({ queryKey: ['cartItems']});
+    },
+  });
+};
+
--- frontend/src/components/cart/CartItem/index.tsx
+++ frontend/src/components/cart/CartItem/index.tsx
@@ -4,90 +4,189 @@ import { colors } from '@styles/colors';
 import styled from '@emotion/styled';
 import Checkbox from '@/components/cart/Checkbox';
 import { IoCloseOutline } from 'react-icons/io5';
-import { FiPlusCircle } from "react-icons/fi";
-import { FiMinusCircle } from "react-icons/fi";
+import { FiPlusCircle } from 'react-icons/fi';
+import { FiMinusCircle } from 'react-icons/fi';
 import { CartType } from '@assets/types/CartType';
 import useCartStore from '@/stores/cartStore';
+import { useChangeQuantityMutation } from '@/apis/hooks/useCartQuery';
+
+import { ErrorIcon } from '@/assets/images/svgs';
+import client from '@/apis/client';
+import { debounce } from 'lodash';
+import { useCallback, useState } from 'react';
+
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { Toast } from '@/components/common/Toast';
+import { Modal } from '@/components/common/Modal';
+import { Button } from '@/components/common/Button';
 
 interface CartItemProps {
   item: CartType;
   index: number | string;
 }
 
-const CartItem = ({item, index}: CartItemProps) => {
+const CartItem = ({ item, index }: CartItemProps) => {
   const toggleSelectItem = useCartStore((state) => state.toggleSelectItem);
   const deleteItem = useCartStore((state) => state.deleteItem);
-  const isChecked = useCartStore((state) => 
-    state.selectedItems.some((selectedItem) => selectedItem.productId === item.productId));
-  const increaseQuantity = useCartStore((state) => state.increaseQuantity);
-  const decreaseQuantity = useCartStore((state) => state.decreaseQuantity);
+  const isChecked = useCartStore((state) =>
+    state.selectedItems.some(
+      (selectedItem) => selectedItem.productId === item.productId,
+    ),
+  );
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const toggleModal = () => {
+    setIsModalOpen((prev) => !prev);
+  };
+
+  const { mutate: changeQuantity } = useChangeQuantityMutation();
 
   const itemPrice = (item.price * item.quantity).toLocaleString('ko-kR');
 
-  return (
-      <CartItemWrapper
-        backgroundColor={colors.White}
-        padding="24px 0"
-        direction="column"
-        height='fit-content'
-        justify='flex-start'
-      >
-        <Flex align="flex-start" padding="0 16px">
-          <Label margin="0 10px 0 0">
-            <Checkbox isChecked={isChecked} handleSelect={() => toggleSelectItem(item)} size={20} />
-          </Label>
-          <Flex direction="column">
-            <Flex justify="space-between" align="flex-start">
-              <Image src={item.productImg} />
-              <Flex
-                direction="column"
-                align="flex-start"
-                padding="0 0 0 14px"
-                gap={16}
-              >
-                <Title typo="Label1" colorCode={colors.Gray700}>
-                  {item.title}
-                </Title>
-                  <Flex
-                    justify="flex-start"
-                    gap={12}
-                  >
-                    <CartItemButton
-                      width='auto'
-                      height='auto'
-                      onClick={() => increaseQuantity(item.productId)}
-                    >
-                      <FiPlusCircle color={colors.Gray400} size={20} />
-                    </CartItemButton>
-                    <Text typo="Label2" colorCode={colors.Gray600}>{item.quantity}</Text>
-                    <CartItemButton
-                      width='auto'
-                      height='auto'
-                      onClick={() => decreaseQuantity(item.productId)}
-                    >
-                      <FiMinusCircle color={colors.Gray400} size={20} />
-                    </CartItemButton>
-                  </Flex>
-              </Flex>
-              <CartItemButton
-                justify="flex-end"
-                width="auto"
-                height="auto"
-                margin="0 0 0 20px"
-                align='flex-start'
-                onClick={() => deleteItem(item.productId)}
-              >
-                <IoCloseOutline size={20} color={colors.Gray400} />
-              </CartItemButton>
-            </Flex>
-            <Flex justify="flex-end" padding='12px 0 0'>
-              <CartText typo="Heading4">{itemPrice}원</CartText>
-            </Flex>
-          </Flex>
-        </Flex>
-      </CartItemWrapper>
+  const debouncedChangeQuantity = debounce(
+    (changeQuantity, productId, quantity) => {
+      changeQuantity({ productId, quantity });
+    },
+    150, // 150ms 후에 마지막 호출만 실행
   );
-}
+
+  const notify = (msg: string) => {
+    toast(
+      <Flex justify="space-between">
+        <span>{msg}</span>
+        <ErrorIcon width={24} height={24} style={{ marginLeft: '8px' }} />
+      </Flex>,
+      {
+        position: 'bottom-center',
+      },
+    );
+  };
+
+  const handleChangeQuantity = useCallback(
+    (diff: number) => () => {
+      const newQuantity = item.quantity + diff;
+      if (newQuantity <= 0 || newQuantity > 99) {
+        return;
+      }
+      // 디바운스 적용된 changeQuantity 호출
+      debouncedChangeQuantity(changeQuantity, item.productId, newQuantity);
+    },
+    [item, changeQuantity], // 디펜던시 배열에 필요한 값 추가
+  );
+
+  const handleDeleteItem = (productId: string) => async () => {
+    deleteItem(productId); // 상태에서 아이템 삭제
+    const deleteData = { productId };
+    try {
+      const response = await client.delete('/cart/remove', {
+        data: deleteData, // 요청 본문에 데이터 포함
+      });
+      if (response) {
+        notify('상품이 삭제되었습니다.');
+      }
+    } catch (error) {
+      notify('상품 삭제에 실패했습니다.');
+      console.error(error);
+    }
+  };
+
+  return (
+    <CartItemWrapper
+      backgroundColor={colors.White}
+      padding="24px 0"
+      direction="column"
+      height="fit-content"
+      justify="flex-start"
+    >
+      <Flex align="flex-start" padding="0 16px">
+        <Label margin="0 10px 0 0">
+          <Checkbox
+            isChecked={isChecked}
+            handleSelect={() => toggleSelectItem(item)}
+            size={20}
+          />
+        </Label>
+        <Flex direction="column">
+          <Flex justify="space-between" align="flex-start">
+            <Image src={item.productImg} />
+            <Flex
+              direction="column"
+              align="flex-start"
+              padding="0 0 0 14px"
+              gap={16}
+            >
+              <Title typo="Label1" colorCode={colors.Gray700}>
+                {item.title}
+              </Title>
+              <Flex justify="flex-start" gap={12}>
+                <CartItemButton
+                  width="auto"
+                  height="auto"
+                  onClick={handleChangeQuantity(-1)}
+                >
+                  <FiMinusCircle color={colors.Gray400} size={20} />
+                </CartItemButton>
+                <Text typo="Label2" colorCode={colors.Gray600}>
+                  {item.quantity}
+                </Text>
+                <CartItemButton
+                  width="auto"
+                  height="auto"
+                  onClick={handleChangeQuantity(+1)}
+                >
+                  <FiPlusCircle color={colors.Gray400} size={20} />
+                </CartItemButton>
+              </Flex>
+            </Flex>
+            <CartItemButton
+              justify="flex-end"
+              width="auto"
+              height="auto"
+              margin="0 0 0 20px"
+              align="flex-start"
+              // onClick={() => deleteItem(item.productId)}
+              onClick={toggleModal}
+            >
+              <IoCloseOutline size={20} color={colors.Gray400} />
+            </CartItemButton>
+            {isModalOpen && (
+              <Modal isOpen={isModalOpen} toggleModal={toggleModal}>
+                <Flex direction="column" padding="32px 0 0" gap={12}>
+                  <Text typo="Heading4">해당 상품을 삭제하시겠습니까?</Text>
+                  <Flex padding="0px 52px" margin="20px 0 10px" gap={20}>
+                    <Button
+                      height="40px"
+                      bg={colors.Gray400}
+                      onClick={() => {
+                        toggleModal();
+                      }}
+                    >
+                      취소
+                    </Button>
+                    <Button
+                      height="40px"
+                      onClick={() => {
+                        handleDeleteItem(item.productId)();
+                        toggleModal();
+                      }}
+                    >
+                      삭제
+                    </Button>
+                  </Flex>
+                </Flex>
+              </Modal>
+            )}
+          </Flex>
+          <Flex justify="flex-end" padding="12px 0 0">
+            <CartText typo="Heading4">{itemPrice}원</CartText>
+          </Flex>
+          {/* <Toast /> */}
+        </Flex>
+      </Flex>
+    </CartItemWrapper>
+  );
+};
 
 const Label = styled.label<{
   margin?: string;
