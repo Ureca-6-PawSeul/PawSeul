647dc820ea554556522a85bd98d4795d674698f0
DongHyeonWon <dhe7700@naver.com>
2024-11-08 11:37:02 +0900

    chore: 주석 처리

--- backend/src/health/health.module.ts
+++ backend/src/health/health.module.ts
@@ -4,7 +4,7 @@ import { HealthController } from './health.controller';
 import { Health } from 'src/entity/health.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Pet } from 'src/entity/pet.entity';
-import { HealthInitService } from 'src/health/init/healthInit.service';
+// import { HealthInitService } from 'src/health/init/healthInit.service';
 import { Product } from 'src/entity/product.entity';
 import { RecommendProduct } from 'src/entity/recommendProduct.entity';
 import { User } from 'src/entity/user.entity';
@@ -14,6 +14,9 @@ import { User } from 'src/entity/user.entity';
     TypeOrmModule.forFeature([Health, Pet, Product, RecommendProduct, User]),
   ],
   controllers: [HealthController],
-  providers: [HealthService, HealthInitService],
+  providers: [
+    HealthService,
+    // HealthInitService
+  ],
 })
 export class HealthModule {}
